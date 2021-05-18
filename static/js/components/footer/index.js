b56381e17ef29b31da350a5c4783a3ffc5992079
Felipe Zuntini <felipezuntini@hotmail.com>
2021-05-17 23:59:59 -0300

    fix (frontend) footer links open in _blank now

--- static/js/components/footer/index.js
+++ static/js/components/footer/index.js
@@ -10,8 +10,8 @@ export default (props) => {
 			<div class="mdl-mini-footer__left-section">
 				<p class="text-white mb-1">Fire Emblem Heroes Character List</p>
 				<ul class="mdl-mini-footer__link-list">
-					<li><a href="https://about.me/zuntini">Author</a></li>
-					<li><a href="https://github.com/tweenn/fire-emblem-heroes-character-list">GitHub</a></li>
+					<li><a href="https://about.me/zuntini" target="_blank">Author</a></li>
+					<li><a href="https://github.com/tweenn/fire-emblem-heroes-character-list" target="_blank">GitHub</a></li>
 				</ul>
 			</div>
 		</footer>
