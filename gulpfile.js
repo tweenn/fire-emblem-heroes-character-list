7e430794199346462567558bc52ce64d0d4ee649
Felipe Zuntini <felipezuntini@hotmail.com>
2021-05-17 22:07:51 -0300

    feat (gulp) minify images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,33 @@
 const gulp = require('gulp');
 const serve = require('gulp-serve');
 const surge = require('gulp-surge');
+const imagemin = require('gulp-imagemin');
+
+const webp = require('gulp-webp');
+
+gulp.task('img:webp', () => {
+	return gulp.src('./static/**/*.webp')
+		.pipe(webp({
+			quality: 100,
+			alphaQuality: 100,
+			method: 6,
+			sns: 100,
+			lossless: true
+		}))
+		.pipe(gulp.dest('./static/'));
+});
+
+gulp.task('img:png', () => {
+	return gulp.src('./static/**/*.png')
+		.pipe(imagemin([
+			imagemin.optipng({
+				optimizationLevel: 7
+			})
+		]))
+		.pipe(gulp.dest('./static/'));
+});
+
+gulp.task('img', gulp.parallel('img:webp', 'img:png'));
 
 gulp.task('serve', serve({
 	root: ['./static'],
